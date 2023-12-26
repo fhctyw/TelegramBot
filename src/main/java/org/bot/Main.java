3f7b663044c579dace385d92a5863c1d87a3f506
fhctyw <lisnychukarsenw@gmail.com>
2023-12-26 18:10:03 +0200

    fix botToken condition

--- src/main/java/org/bot/Main.java
+++ src/main/java/org/bot/Main.java
@@ -25,7 +25,7 @@ public class Main {
     }
     public static void main(final String[] args) {
         try {
-            final String botToken = (args.length == 2 && args[0].isEmpty())
+            final String botToken = (args.length > 2 && !args[0].isEmpty())
                     ? args[0] : readResource("token");
             final TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
             api.registerBot(new MessageBot(botToken));
